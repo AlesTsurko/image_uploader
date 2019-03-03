521a8a179c309e3897136c47cd7cf8f5b467bb18
Ales Tsurko <ales.tsurko@gmail.com>
2019-03-03 21:46:00 +0300

    prototype UploadHandler::choose_strategy; refactor

--- src/upload_handler.rs
+++ src/upload_handler.rs
@@ -3,33 +3,50 @@ use actix_web::{
     Error,
     HttpRequest,
     HttpResponse,
+    HttpMessage,
     error,
     dev::Handler,
+    error::Result as ActixResult,
+    AsyncResponder,
 };
-use futures::Future;
+use futures::future::{Future, result};
 use direct_strategy::DirectStrategy;
+use crate::AppState;
 
 type HandlerResult = Box<Future<Item = HttpResponse, Error = Error>>;
 
 pub struct UploadHandler;
 
-impl<S> Handler<S> for UploadHandler {
+impl Handler<AppState> for UploadHandler {
     type Result = HandlerResult;
 
-    fn handle(&self, req: &HttpRequest<S>) -> Self::Result {
-        let strategy = self.choose_strategy(req);
+    fn handle(&self, req: &HttpRequest<AppState>) -> Self::Result {
+        let strategy = match self.choose_strategy(req) {
+            Ok(s) => s,
+            Err(e) => return result(Err(e)).responder(),
+        };
         strategy.handle_request(req)
     }
 }
 
 impl UploadHandler {
-    fn choose_strategy<S>(&self, req: &HttpRequest<S>) -> impl Strategy {
-        DirectStrategy {}
+    fn choose_strategy(&self, req: &HttpRequest<AppState>) -> ActixResult<impl Strategy> {
+        let mime = match req.mime_type()? {
+            Some(t) => t,
+            None => return Err(error::ErrorBadRequest("MIME not specified"))
+        };
+
+        match (mime.type_(), mime.subtype()) {
+            (mime::APPLICATION, mime::JSON) => Ok(DirectStrategy {}),
+            (mime::MULTIPART, mime::FORM_DATA) => Ok(DirectStrategy {}),
+            (mime::IMAGE, _) => Ok(DirectStrategy {}),
+            _ => Err(error::ErrorBadRequest("Unsupported MIME type")),
+        }
     }
 }
 
 trait Strategy {
-    fn handle_request<S>(&self, req: &HttpRequest<S>) -> HandlerResult;
+    fn handle_request(&self, req: &HttpRequest<AppState>) -> HandlerResult;
 }
 
 fn std_error_into_internal_server(e: std::io::Error) -> Error {
--- src/upload_handler/direct_strategy.rs
+++ src/upload_handler/direct_strategy.rs
@@ -13,7 +13,10 @@ use serde_json::{
 };
 use std::fs::{File, create_dir_all};
 use std::io::Write;
-use crate::ImageType;
+use crate::{
+    ImageType, 
+    AppState,
+};
 use super::{
     HandlerResult, 
     Strategy,
@@ -24,23 +27,24 @@ use bytes::Bytes;
 pub struct DirectStrategy;
 
 impl Strategy for DirectStrategy {
-    fn handle_request<S>(&self, req: &HttpRequest<S>) -> HandlerResult {
+    fn handle_request(&self, req: &HttpRequest<AppState>) -> HandlerResult {
         let mime_type = req.mime_type();
+        let app_state = req.state().clone();
 
         req.payload()
             .concat2()
             .from_err()
-            .and_then(|body| DirectStrategy::respond_for_body_with_mime_type(&body, &mime_type?))
+            .and_then(|body| DirectStrategy::respond_for_body_with_mime_type(app_state, &body, &mime_type?))
             .responder()
     }
 }
 
 impl DirectStrategy {
-    fn respond_for_body_with_mime_type(body: &Bytes, mime_type: &Option<mime::Mime>) -> ActixResult<HttpResponse> {
+    fn respond_for_body_with_mime_type(app_state: AppState, body: &Bytes, mime_type: &Option<mime::Mime>) -> ActixResult<HttpResponse> {
         let image_type = DirectStrategy::get_image_type_from_mime_type(&mime_type)?;
         DirectStrategy::check_image_type(&image_type)?;
 
-        DirectStrategy::write_image_from_content_with_type(body, &image_type)?;
+        DirectStrategy::write_image_from_content_with_type(body, &image_type, &app_state.storage_path)?;
 
         let response = DirectStrategy::prepare_succesful_response();
 
@@ -61,15 +65,22 @@ impl DirectStrategy {
         Ok(())
     }
 
-    fn write_image_from_content_with_type(content: &Bytes, image_type: &ImageType) -> ActixResult<()> {
-        create_dir_all("storage")
+    fn write_image_from_content_with_type(content: &Bytes, image_type: &ImageType, storage_path: &str) -> ActixResult<()> {
+        create_dir_all(storage_path)
             .map_err(std_error_into_internal_server)?;
 
-        let mut file = File::create(&format!("storage/image.{}", image_type.to_string()))
+        let file_path = DirectStrategy::get_image_path_for_storage_path_and_image_type(image_type, storage_path)?;
+        let mut file = File::create(&file_path)
             .map_err(std_error_into_internal_server)?;
 
         file.write_all(&content)
             .map_err(std_error_into_internal_server)
+    }
+
+    fn get_image_path_for_storage_path_and_image_type(image_type: &ImageType, storage_path: &str) -> ActixResult<String> {
+        //TODO generate id
+        let id = "image";
+        Ok(format!("{}/{}.{}", storage_path, id, image_type.to_string()))
     }
 
     fn prepare_succesful_response() -> JsonValue {
